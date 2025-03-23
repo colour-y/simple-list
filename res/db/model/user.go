9c54040f500e8bb652c66b5a95d09f3edde69454
夏尾草 <1910541664@qq.com>
2025-03-23 22:46:01 +0800

    完成了model/user部分

---
+++ res/db/model/user.go
@@ -0,0 +1,30 @@
+package model
+
+import (
+	"github.com/jinzhu/gorm"
+	"golang.org/x/crypto/bcrypt"
+)
+
+type User struct {
+	gorm.Model
+	UserName       string `gorm:"unique"`
+	PasswordDigest string
+}
+
+const (
+	passwordcost = 12
+)
+
+func (user *User) SetPassword(password string) error {
+	bytes, err := bcrypt.GenerateFromPassword([]byte(password), passwordcost)
+	if err != nil {
+		return nil
+	}
+	user.PasswordDigest = string(bytes)
+	return nil
+}
+
+func (user *User) CheckPassword(password string) bool {
+	err := bcrypt.CompareHashAndPassword([]byte(user.PasswordDigest), []byte(password))
+	return err == nil
+}
