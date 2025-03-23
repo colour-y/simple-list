bed1322ebd6e4eba3a7ec8468fc0cf1bfb2bf99d
夏尾草 <1910541664@qq.com>
2025-03-24 02:12:09 +0800

    累了睡觉

---
+++ pkg/util/logger.go
@@ -0,0 +1,59 @@
+package util
+
+import (
+	"github.com/sirupsen/logrus"
+	"log"
+	"os"
+	"path"
+	"time"
+)
+
+var LogrusObj *logrus.Logger
+
+func Initlog() {
+	if LogrusObj != nil {
+		src, _ := setOutputfile()
+		LogrusObj.Out = src
+		return
+	}
+
+	logger := logrus.New()
+	src, _ := setOutputfile()
+	logger.Out = src
+	logger.SetLevel(logrus.DebugLevel)
+
+	logger.SetFormatter(&logrus.TextFormatter{
+		TimestampFormat: "2025-03-24",
+	})
+	LogrusObj = logger
+}
+
+func setOutputfile() (*os.File, error) {
+	now := time.Now()
+	logFilePath := ""
+	if dir, err := os.Getwd(); err == nil {
+		logFilePath = dir + "/logs/"
+	}
+	_, err := os.Stat(logFilePath)
+	if os.IsNotExist(err) {
+		if err := os.MkdirAll(logFilePath, 0777); err != nil {
+			log.Println(err.Error())
+			return nil, err
+		}
+	}
+	logFileName := now.Format("2006-01-02") + ".log"
+	// 日志文件
+	fileName := path.Join(logFilePath, logFileName)
+	if _, err := os.Stat(fileName); err != nil {
+		if _, err := os.Create(fileName); err != nil {
+			log.Println(err.Error())
+			return nil, err
+		}
+	}
+	src, err := os.OpenFile(fileName, os.O_APPEND|os.O_WRONLY, os.ModeAppend)
+	if err != nil {
+		log.Println(err)
+		return nil, err
+	}
+	return src, nil
+}
